071361fc08a7be86efd34ab5c41b0a2bf14c2fb0
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-01-26 20:12:48 -0600

    Update Guia 1

---
+++ Guia2/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Header } from './components/Header'
+import { Footer } from './components/footer'
+import { db } from './data/db'
+import { Guitar } from './components/Guitar'
+
+function App() {
+
+  const[data, setData]=useState(db)
+  console.log(data)
+
+  return (
+    <>
+    <Header></Header>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+            <Guitar></Guitar>
+            <Guitar></Guitar>
+        </div>
+    </main>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
