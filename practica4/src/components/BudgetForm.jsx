44fae0c777f0d8155fc14e9294b881402e0321d6
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-02-25 16:34:02 -0600

    update guia 4

--- practica4/src/components/BudgetForm.jsx
+++ practica4/src/components/BudgetForm.jsx
@@ -4,7 +4,7 @@ export const BudgetForm = () => {
     const [buget, setBudget]= useState(0)
     const isInvalid = isNaN(buget) || buget<=0 //si no es un numero >0 es invalido
 
-    const handleChange =(e) =>{setBudget(e.target.valueAsNumber)}
+    const handleChange=(e)=>{setBudget(e.target.valueAsNumber)}
 
     return(
         <form className="space-y-5">
@@ -14,21 +14,21 @@ export const BudgetForm = () => {
                 </label>
             </div>
             <input 
-            type="number"
-            id="budget"
-            name="budget"
-            placeholder="Define tu presupuesto"
-            className="w-full bg-white border border-gray-200 p-2"
-            value={budget}
-            onChange={handleChange}
+                type="number"
+                id="budget"
+                name="budget"
+                placeholder="Define tu presupuesto"
+                className="w-full bg-white border border-gray-200 p-2"
+                value={budget}
+                onChange={handleChange}
             />
             <input 
-            type="submit"
-            value="Definir presupuesto"
-            disabled={isInvalid}
-            className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40" />
+                type="submit"
+                value="Definir presupuesto"
+                disabled={isInvalid}
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40" />
         </form>
     )
 }
 
-export default BudgetForm;
+export default BudgetForm
--- practica4/src/main.jsx
+++ practica4/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import BudgetForm from './components/BudgetForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <BudgetForm></BudgetForm>
   </StrictMode>,
 )
