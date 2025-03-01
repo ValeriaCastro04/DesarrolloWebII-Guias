5c9a18f36a07245d4d1ad4a89075676be420dd21
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-01 13:56:43 -0600

    Update App.jsx

--- practica4/src/App.jsx
+++ practica4/src/App.jsx
@@ -1,17 +1,7 @@
-import BudgetForm from "./components/BudgetForm";
-
-function App(){
-    return(
+export const App = () => {
+    return (
         <>
-            <header className="bg-blue-600 py-8 max-h-72">
-                <h1 className="uppercase text-center font-black text-4xl text-white">
-                    Planificador de gastos
-                </h1>
-            </header>
-            <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-                <BudgetForm/>
-            </div>
+        <h1 className="text-8xl">Control de gastos</h1>
         </>
     )
 }
-export default App
