45444daa81424d104b0bdfe07d19cfb29cc5602f
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-01 20:33:42 -0600

    Update ExpenseModal.jsx

--- practica4/src/components/ExpenseModal.jsx
+++ practica4/src/components/ExpenseModal.jsx
@@ -15,11 +15,37 @@ export default function ExpenseModal(){
             type='button'
             onClick={()=>dispatch({type:'show-modal'})}
             >
-                <PlusCircleIcon className='w-16'/>
-
+                <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full'/>
             </button>
-
         </div>
+
+        <Transition appear show={modal} as={Fragment}>
+            <Dialog as='div' className="relative z-10" onClose={() => 
+                dispatch({type:'close-modal'})}>
+                <Transition.Child
+                    as={Fragment}
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                >
+                    <div className='fixed inset-0 bg-black bg-opacity-75'/>
+                </Transition.Child>
+
+                <div className='fixed inset-0 overflow-y-auto'>
+                    <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                        <Transition.Child>
+                            
+                        </Transition.Child>
+
+                    </div>
+
+                </div>
+
+            </Dialog>
+        </Transition>
         </>
     )
 
