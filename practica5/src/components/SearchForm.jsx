3389908e61da5ffdc6f076b89dceb7059c5839f1
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-07 09:46:34 -0600

    update hasta paso 43

--- practica5/src/components/SearchForm.jsx
+++ practica5/src/components/SearchForm.jsx
@@ -1,21 +1,51 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAppStore } from "../store/useAppStore"
 
-export default function SearchForm(){
-    const fetchCategories=useAppStore((state)=>state.fetchCategories)
-    const categories=useAppStore((state)=>state.categories)
+export default function SearchForm() {
+    const fetchCategories = useAppStore((state) => state.fetchCategories)
+    const categories = useAppStore((state) => state.categories)
 
-    useEffect(()=>{
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    })
+
+    const handleChange = (e) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const searchRecipes = useAppStore((state) => state.searchRecipes)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // Validar que no haya campos vacíos
+        if (Object.values(searchFilters).includes('')) {
+            console.log('Todos los campos son obligatorios')
+            return
+        }
+
+        // Consultar las recetas
+        searchRecipes(searchFilters)
+    }
+
+    useEffect(() => {
         fetchCategories()
-    },[])
+    }, [])
 
-    return(
+    return (
         <form 
-            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-16 p-10 rounded-lg shadow-space-y-6">
+            onSubmit={handleSubmit}  
+            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-16 p-10 rounded-lg shadow-space-y-6"
+        >
             <div className="space-y-4">
                 <label 
-                htmlFor="ingredient"
-                className="block text-white uppercase font-extrabold text-lg">
+                    htmlFor="ingredient"
+                    className="block text-white uppercase font-extrabold text-lg"
+                >
                     Nombre o Ingredientes
                 </label>
 
@@ -23,32 +53,41 @@ export default function SearchForm(){
                     id="ingredient"
                     type="text"
                     name="ingredient"
+                    value={searchFilters.ingredient}  
+                    onChange={handleChange} 
                     className="p-3 w-full rounded-lg focus:outline-none bg-white"
-                    placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe"/>
+                    placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe"
+                />
             </div>
+
             <div className="space-y-4">
                 <label 
                     htmlFor="category"
-                    className="block text-white uppercase font-extrabold text-lg">
-                        Categoría
-                    </label>
+                    className="block text-white uppercase font-extrabold text-lg"
+                >
+                    Categoría
+                </label>
 
-                    <select 
-                        name="category" 
-                        id="category"
-                        className="p-3 w-full rounded-lg focus:outline-none bg-white">
-                            <option value="">-- Seleccione --</option>
-                            {categories.map(category=>(
-                                <option value={category.strCategory} key={category.strCategory}>
-                                    {category.strCategory}
-                                </option>
-                            ))}
-                    </select>
+                <select 
+                    name="category" 
+                    id="category"
+                    value={searchFilters.category} 
+                    onChange={handleChange}  
+                    className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                >
+                    <option value="">-- Seleccione --</option>
+                    {categories.map(category => (
+                        <option value={category.strCategory} key={category.strCategory}>
+                            {category.strCategory}
+                        </option>
+                    ))}
+                </select>
             </div>
+
             <input 
                 type="submit"
                 value="Buscar Recetas"
-                className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" 
+                className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
             />
         </form>
     )
--- practica5/src/services/RecipeService.js
+++ practica5/src/services/RecipeService.js
@@ -8,3 +8,17 @@ export async function getCategories() {
     console.log(data.drinks)
     return data.drinks;
 }
+
+//funcion para obtener recetas basadas en filtros
+export async function getRecipes(filters) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`;
+    const {data} = await axios(url);
+    return data;
+}
+
+//Funcion para obtener una receta por su ID
+export async function getRecipeById(id) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${id}`;
+    const {data} = await axios(url);
+    return data.drinks ? data.drinks[0] : null;
+}
