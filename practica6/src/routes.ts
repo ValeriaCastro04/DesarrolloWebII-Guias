ccd6f25f4857ba180af21d982c45b98b56a8665f
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-22 09:08:06 -0600

    update guia6

--- practica6/src/routes.ts
+++ practica6/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import User from "./models/Users";
 
 const router = Router();
 
@@ -6,8 +7,10 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).send('Bienvenido a la API');
 });
 
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async (req, res) => {
     console.log(req.body);
+    const user = new User(req.body);
+    await user.save();
     res.status(201).json({message: 'Datos de usuario recibidos con exito'});
 });
 
