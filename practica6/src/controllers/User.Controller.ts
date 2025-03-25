16533f8f1347d298f3a489886e2821935e618125
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-24 19:40:13 -0600

    update guia 6

--- practica6/src/controllers/User.Controller.ts
+++ practica6/src/controllers/User.Controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import User from "../models/Users";
+import { hashPassword } from "../utils/auth";
+import { validationResult } from "express-validator";
 
 export const createAccount = async (req: Request, res: Response) => {
     const {name, password, email, username} = req.body;
+
+    let errors = validationResult(req)
+    console.log(errors)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
 
     const userExists = await User.findOne({email});
     if (userExists) {
@@ -16,6 +24,24 @@ export const createAccount = async (req: Request, res: Response) => {
     }
 
     const user = new User(req.body);
+    user.password = await hashPassword(password);
     await user.save();
     res.status(201).json({message: 'User created successfully'});
+}
+
+export const login = async (req:Request, res: Response) => {
+    //validar los errores en la solicitud
+    let errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    //extraer email y password del cuerpo de la solicitud
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    if (!user){
+        const error = new Error('Invalid credencials')
+        return res.status(401).json({error:error.message})
+    }
+
+    res.status(200).send('Authenticated')
 }
--- practica6/src/routes.ts
+++ practica6/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import User from "./models/Users";
-import { createAccount } from "./controllers/User.Controller";
+import { createAccount, login } from "./controllers/User.Controller";
+import { body } from "express-validator";
 
 const router = Router();
 
@@ -8,6 +9,23 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).send('Bienvenido a la API');
 });
 
-router.post('/auth/register', createAccount);
+router.post('/auth/register', [
+    body("name").isString().notEmpty().withMessage("Name is required"),
+    body("password").isString().isLength({min:8})
+        .withMessage("Password must be at least 8 characters"),
+    body("email").isEmail().withMessage("Invalid email"),
+    body("username").isString().notEmpty().withMessage("Username is required")],
+    async (req, res) => {
+        createAccount(req, res);
+})
+
+router.post('/auth/login',
+    [
+        body("email").isEmail().withMessage("Invalid email"),
+        body("password").notEmpty().withMessage("Password must be at least 8 characters"),
+    ],
+    async (req, res) => {
+        login(req, res);
+})
 
 export default router;
