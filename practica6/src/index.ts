71cafe740efddd7febe2907dba88d160afe8aceb
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-03-24 19:41:16 -0600

    Update index.ts

--- practica6/src/index.ts
+++ practica6/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import router from "./routes";
 import { connectBD } from './config/db';
 
-const PORT = 3000;
+const PORT = 3002;
 const app = express();
 
 app.use(express.json());
